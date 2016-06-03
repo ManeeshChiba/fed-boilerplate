9a707c9fd7cb58aaaf30385c84c20dc56dade47d
Maneesh Chiba <maneesh.chiba@native.co.za>
2016-06-03 08:48:39 +0200

    Added in options for LESS compilation

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,6 +7,7 @@ gulp.task('watch', function() {
 
   gulp.watch('src/*.html',  ['html']);
   gulp.watch('src/scss/**/*.scss',  ['styles','scsslint']);
+  gulp.watch('src/less/**/*.less',  ['styles']);
   gulp.watch('src/img/**/*',  ['images']);
   gulp.watch('src/js/**/*.js',  ['scripts','jshints']);
 });
